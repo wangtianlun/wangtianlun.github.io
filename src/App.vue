2bbb61cdc22946490b343053efefb16144addcdf
wangtianlun <wb-wangtianlun@alibaba-inc.com>
2016-03-09 00:04:22 +0800

    first commit! start vue!

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello Vue!'
+    }
+  }
+
+}
+</script>
+
+<style lang="less">
+  #app {
+    color: red;
+  }
+</style>
